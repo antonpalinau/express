36ed53de63b5c3f060e048f4a1f7d03d1db6d31f
antonpalinau <anton_palinau@epam.com>
2019-12-22 20:34:50 +0300

    fixed type in getautosuggetusers

--- src/routes/api/getAutoSuggestUsers.ts
+++ src/routes/api/getAutoSuggestUsers.ts
@@ -5,7 +5,7 @@ import { IUser } from "../../types";
 const router: Router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    const newUsers = users
+    const newUsers: IUser[] = users
         .filter((user: IUser): boolean => user.login.includes(req.query.loginSubstring))
         .sort((user1: IUser, user2: IUser): number => user1.login.localeCompare(user2.login))
         .slice(0, req.query.limit);
